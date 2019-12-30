7ce1965b4a1c4d47ef8f3b17178d52891f073adb
TENCENT\huanhzhang <huanhzhang@tencent.com>
2019-12-30 10:53:28 +0800

    init

---
+++ src/main/java/Movie.java
@@ -0,0 +1,40 @@
+public class Movie {
+    private String title;
+    Price price;
+
+    public Movie(
+            String title, Type priceCode) {
+        this.title = title;
+        setPriceCode(priceCode);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    private void setPriceCode(
+            Type priceCode) {
+        switch (priceCode) {
+            case CHILDREN:
+                price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                price = new NewReleasePrice();
+                break;
+            case REGULAR:
+                price = new RegularPrice();
+                break;
+            default:
+                throw new IllegalArgumentException(
+                        "invalid price code");
+        }
+    }
+
+    public double getCharge(int daysRented) {
+        return price.getCharge(daysRented);
+    }
+
+    public int getPoints(int daysRented) {
+        return price.getPoints(daysRented);
+    }
+}
---
+++ src/main/java/Type.java
@@ -0,0 +1,3 @@
+public enum Type {
+    REGULAR, NEW_RELEASE, CHILDREN, UNKNOWN;
+}
