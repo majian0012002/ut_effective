e5b2123ba2a7e846d7931ea2d0d3cfa099c03e4f
TENCENT\huanhzhang <huanhzhang@tencent.com>
2020-02-24 19:24:26 +0800

    E4 Replace ObjectMother with DataBuilder 当场景有限且不变时，ObjectMother是一种有效的工具 DataBuilder是基于ObjectMother的场景的替代方案，它还解决了级联更新风险。Data Builder的唯一职责是创建具有默认值的领域对象 任何傻瓜都可以写一个对当下有用的测试；优秀的程序员编写可以在将来帮助整个团队的测试。

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,78 +1,138 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
+    @Test
+    public void getName() {
+        assertEquals(
+                "John",
+                a.customer.w(
+                        "John").build().getName());
+    }
     @Test
     public void noRentalsStatement() {
         assertEquals(
                 "Rental record for David\nAmount " +
-                        "owed is 0.0\n" +
-                        "You earned 0 frequent renter points",
-                ObjectMother
-                        .customerWithNoRentals(
-                                "David").statement());
-    }
-    @Test
-    public void oneNewReleaseStatement() {
-        assertEquals(
-                "Rental record for John\n\t" +
-                        "Godfather 4 9.0\n" +
-                        "Amount owed is 9.0\n" +
-                        "You earned 2 frequent renter points",
-                ObjectMother
-                        .customerWithOneNewRelease(
-                                "John").statement());
+                        "owed is 0.0\nYou earned 0 frequent " +
+                        "renter points",
+                a.customer.w(
+                        "David").build().statement());
     }
 
     @Test
+    public void oneNewReleaseStatement() {
+        assertEquals(
+                "Rental record for John\n" +
+                        "\tGodfather 4 9.0\n" +
+                        "Amount owed is 9.0\n" +
+                        "You earned 2 frequent renter points",
+                a.customer.w("John").w(
+                        a.rental.w(
+                                a.movie.w(
+                                        Type.NEW_RELEASE))).build()
+                        .statement());
+    }
+    @Test
     public void allRentalTypesStatement() {
         assertEquals(
-                "Rental record for Pat\n\t" +
-                        "Godfather 4 9.0\n" +
-                        "\tScarface 3.5\n\tLion King 1.5\n" +
+                "Rental record for Pat\n" +
+                        "\tGodfather 4 9.0\n" +
+                        "\tScarface 3.5\n" +
+                        "\tLion King 1.5\n" +
                         "Amount owed is 14.0\n" +
                         "You earned 4 frequent renter points",
-                ObjectMother
-                        .customerWithOneOfEachRentalType(
-                                "Pat").statement());
+                a.customer.w("Pat").w(
+                        a.rental.w(a.movie.w(Type.NEW_RELEASE)),
+                        a.rental.w(
+                                a.movie.w("Scarface").w(Type.REGULAR)),
+                        a.rental.w(
+                                a.movie.w("Lion King").w(
+                                        Type.CHILDREN))).build().statement());
     }
+
     @Test
     public void
     newReleaseAndRegularStatement() {
         assertEquals(
-                "Rental record for Steve\n\t" +
-                        "Godfather 4 9.0\n" +
+                "Rental record for Steve\n" +
+                        "\tGodfather 4 9.0\n" +
                         "\tScarface 3.5\n" +
                         "Amount owed is 12.5\n" +
                         "You earned 3 frequent renter points",
-                ObjectMother
-                        .customerWithOneNewReleaseAndOneRegular(
-                                "Steve").statement());
+                a.customer.w("Steve").w(
+                        a.rental.w(a.movie.w(Type.NEW_RELEASE)),
+                        a.rental.w(
+                                a.movie.w("Scarface").w(
+                                        Type.REGULAR))).build().statement());
+    }
+    @Test
+    public void noRentalsHtmlStatement() {
+        assertEquals(
+                "<h1>Rental record for <em>David" +
+                        "</em></h1>\n<p>Amount owed is <em>" +
+                        "0.0</em></p>\n<p>You earned <em>0 " +
+                        "frequent renter points</em></p>",
+                a.customer.w(
+                        "David").build().htmlStatement());
     }
 
     @Test
-    public void patHtmlStatement() {
+    public void oneNewReleaseHtmlStatement() {
         assertEquals(
-                "<h1>Rental record for <em>Pat</em></h1>\n" +
-                        "<p>Godfather 4 9.0</p>\n" +
-                        "<p>Scarface 3.5</p>\n" +
-                        "<p>Lion King 1.5</p>\n" +
-                        "<p>Amount owed is <em>14.0</em></p>\n" +
-                        "<p>You earned <em>4 frequent renter points</em></p>",
-                ObjectMother
-                        .customerWithOneOfEachRentalType(
-                                "Pat").htmlStatement());
+                "<h1>Rental record for <em>John</em>" +
+                        "</h1>\n<p>Godfather 4 9.0</p>\n" +
+                        "<p>Amount owed is <em>9.0</em></p>" +
+                        "\n<p>You earned <em>2 frequent " +
+                        "renter points</em></p>",
+                a.customer.w("John").w(
+                        a.rental.w(
+                                a.movie.w(
+                                        Type.NEW_RELEASE))).build()
+                        .htmlStatement());
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
+    public void allRentalTypesHtmlStatement() {
+        assertEquals(
+                "<h1>Rental record for <em>Pat</em>" +
+                        "</h1>\n<p>Godfather 4 9.0</p>\n" +
+                        "<p>Scarface 3.5</p>\n<p>Lion King" +
+                        " 1.5</p>\n<p>Amount owed is <em>" +
+                        "14.0</em></p>\n<p>You earned <em>" +
+                        "4 frequent renter points</em></p>",
+                a.customer.w("Pat").w(
+                        a.rental.w(a.movie.w(Type.NEW_RELEASE)),
+                        a.rental.w(
+                                a.movie.w("Scarface").w(Type.REGULAR)),
+                        a.rental.w(
+                                a.movie.w("Lion King").w(
+                                        Type.CHILDREN))).build()
+                        .htmlStatement());
+    }
+
+    @Test
+    public void
+    newReleaseAndRegularHtmlStatement() {
+        assertEquals(
+                "<h1>Rental record for <em>Steve" +
+                        "</em></h1>\n<p>Godfather 4 9.0</p>" +
+                        "\n<p>Scarface 3.5</p>\n<p>Amount " +
+                        "owed is <em>12.5</em></p>\n<p>" +
+                        "You earned <em>3 frequent renter " +
+                        "points</em></p>",
+                a.customer.w("Steve").w(
+                        a.rental.w(a.movie.w(Type.NEW_RELEASE)),
+                        a.rental.w(
+                                a.movie.w("Scarface").w(
+                                        Type.REGULAR))).build()
+                        .htmlStatement());
+    }
+    @Test
+            (expected=IllegalArgumentException.class)
     public void invalidTitle() {
-        ObjectMother
-                .customerWithNoRentals("Bob")
-                .addRental(
-                        new Rental(
-                                new Movie("Crazy, Stupid, Love.",
-                                        Type.UNKNOWN),
-                                4));
+        a.customer.w(
+                a.rental.w(
+                        a.movie.w(Type.UNKNOWN))).build();
     }
-
 }
