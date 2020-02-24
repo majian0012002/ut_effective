a54cfcab0741f330906b878032a7e292551eab33
TENCENT\huanhzhang <huanhzhang@tencent.com>
2020-02-24 17:46:36 +0800

    E3. 内联setup: 在Setup中创建david、john、pat和steve的实例会将字符移出单独的测试方法，但是不会为我们提供任何其他优势。通过添加间接层，我们从测试中删除了些字符，但是却我们强迫自己记住谁拥有什么Rentals。删除setup方法几乎总是揭示了局部或全局改进的机会。 在这种情况下，通过删除setup，我们可以进一步限制需要检查的变量的数量。随着setup的删除，您不再需要寻找setup，也不再需要关心与无关的Customer实例。 通过内联Setup，我们可以删除Setup方法和Customer字段。我们的测试看起来很漂亮，而且它们几乎不需要导航就能完全理解。 我继续重命名测试，删除未使用的Customer字段，并内联单个usage field。

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,72 +1,53 @@
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-
 public class CustomerTest {
-    Customer john, steve, pat, david;
-    String johnName = "John",
-            steveName = "Steve",
-            patName = "Pat",
-            davidName = "David";
-    Customer[] customers;
-
-    @Before
-    public void setup() {
-        david = ObjectMother
-                .customerWithNoRentals(davidName);
-        john = ObjectMother
-                .customerWithOneNewRelease(johnName);
-        pat = ObjectMother
-                .customerWithOneOfEachRentalType(
-                        patName);
-        steve = ObjectMother
-                .customerWithOneNewReleaseAndOneRegular(
-                        steveName);
-        customers = new Customer[]{
-                david, john, steve, pat};
-    }
-
     @Test
-    public void davidStatement() {
+    public void noRentalsStatement() {
         assertEquals(
                 "Rental record for David\nAmount " +
                         "owed is 0.0\n" +
                         "You earned 0 frequent renter points",
-                david.statement());
+                ObjectMother
+                        .customerWithNoRentals(
+                                "David").statement());
     }
-
     @Test
-    public void johnStatement() {
+    public void oneNewReleaseStatement() {
         assertEquals(
                 "Rental record for John\n\t" +
-                        "Godfather 4\t9.0\n" +
+                        "Godfather 4 9.0\n" +
                         "Amount owed is 9.0\n" +
                         "You earned 2 frequent renter points",
-                john.statement());
+                ObjectMother
+                        .customerWithOneNewRelease(
+                                "John").statement());
     }
 
     @Test
-    public void patStatement() {
+    public void allRentalTypesStatement() {
         assertEquals(
                 "Rental record for Pat\n\t" +
-                        "Godfather 4\t9.0\n" +
-                        "\tScarface\t3.5\n" +
-                        "\tLion King\t1.5\n" +
+                        "Godfather 4 9.0\n" +
+                        "\tScarface 3.5\n\tLion King 1.5\n" +
                         "Amount owed is 14.0\n" +
                         "You earned 4 frequent renter points",
-                pat.statement());
+                ObjectMother
+                        .customerWithOneOfEachRentalType(
+                                "Pat").statement());
     }
-
     @Test
-    public void steveStatement() {
+    public void
+    newReleaseAndRegularStatement() {
         assertEquals(
                 "Rental record for Steve\n\t" +
-                        "Godfather 4\t9.0\n" +
-                        "\tScarface\t3.5\n" +
+                        "Godfather 4 9.0\n" +
+                        "\tScarface 3.5\n" +
                         "Amount owed is 12.5\n" +
                         "You earned 3 frequent renter points",
-                steve.statement());
+                ObjectMother
+                        .customerWithOneNewReleaseAndOneRegular(
+                                "Steve").statement());
     }
 
     @Test
@@ -78,7 +59,9 @@ public class CustomerTest {
                         "<p>Lion King 1.5</p>\n" +
                         "<p>Amount owed is <em>14.0</em></p>\n" +
                         "<p>You earned <em>4 frequent renter points</em></p>",
-                pat.htmlStatement());
+                ObjectMother
+                        .customerWithOneOfEachRentalType(
+                                "Pat").htmlStatement());
     }
 
     @Test(expected = IllegalArgumentException.class)
