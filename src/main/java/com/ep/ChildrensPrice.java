158b9bb1ff3a2977c9234853005d17457aa9f881
huanhzhang <huanhzhang@tencent.com>
2020-02-28 17:41:03 +0800

    MovieTest中的用例getChargeForChildrens违反了One Assertion Per Test规则，一个用例中有多个断言。测试失败时提供的信息没有多少价值，需要去看stacktrace来定位 对于带有多个断言的测试，当它们都通过时，它们都提供价值; 但是，当断言失败时，它将删除所有后续断言提供的任何价值。 您必须阅读领域代码和测试代码，以确定任何附加的断言是否会失败；或者您必须在运行测试和修复任何新的失败之间进行来回往返。 这两种方法都不如只运行一次测试并显示所有通过/失败信息

--- src/main/java/com/ep/ChildrensPrice.java
+++ src/main/java/com/ep/ChildrensPrice.java
@@ -3,8 +3,8 @@ public class ChildrensPrice extends Price {
     @Override
     double getCharge(int daysRented) {
         double amount = 1.5;
-        if (daysRented > 3)
-            amount += (daysRented - 3) * 1.5;
+        if (daysRented > 2) // *was 3*
+            amount += (daysRented - 2) * 1.5;
         return amount;
     }
 }
