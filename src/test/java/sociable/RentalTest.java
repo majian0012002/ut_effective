7353879123307274a6ea34a22bc311f0333cb6c4
huanhzhang <huanhzhang@tencent.com>
2020-03-02 14:43:11 +0800

    三个Negative Testing的例子，第一个storeMockNeverReceivesRemove看起来最合理；第二个failOnStoreRemove使用了2个具体的类，不是solitary测试，且违反了Assert Last法则 三个测试最大问题是：如果 Rental.start()的实现有变化，例如不再调用remove(movie)，而是另一个overload的remove()方法，这三个测试都不会失败。 Negative Testing最大的问题: 它们保证没有发生什么事情，但是它们不能保证系统按照预期进行集成 我不确定这些可疑的测试有什么价值;但是，如果您必须编写类似的内容，那么有一些方法可以获得比上述示例更多的价值。

--- src/test/java/sociable/RentalTest.java
+++ src/test/java/sociable/RentalTest.java
@@ -3,6 +3,9 @@ package sociable;
 import org.junit.Test;
 import com.ep.*;
 import builder.a;
+
+import java.util.HashMap;
+
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 public class RentalTest {
@@ -66,5 +69,49 @@ public class RentalTest {
         verify(
                 store, never()).remove(
                 any(Movie.class));
+    }
+
+    @Test
+    public void
+    storeMockNeverReceivesRemove() {
+        Movie movie = mock(Movie.class);
+        Rental rental =
+                a.rental.w(movie).build();
+        Store store = mock(Store.class);
+        when(
+                store.getAvailability(
+                        any(Movie.class)))
+                .thenReturn(0);
+        rental.start(store);
+        verify(store, never()).remove(movie);
+    }
+    @Test
+    public void failOnStoreRemove() {
+        Movie movie = mock(Movie.class);
+        Rental rental =
+                a.rental.w(movie).build();
+        Store store = new Store(
+                new HashMap<Movie,Integer>()) {
+            public void remove(Movie movie) {
+                fail();
+            }
+        };
+        rental.start(store);
+    }
+
+    @Test
+    public void storeShouldNeverRemove() {
+        final boolean[] removeCalled = { false };
+        Movie movie = mock(Movie.class);
+        Rental rental =
+                a.rental.w(movie).build();
+        Store store = new Store(
+                new HashMap<Movie,Integer>()) {
+            public void remove(Movie movie) {
+                removeCalled[0] = true;
+            }
+        };
+        rental.start(store);
+        assertFalse(removeCalled[0]);
     }
 }
