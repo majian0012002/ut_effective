2310b6b63474460ef1fa8a107888a3af3315cfb4
huanhzhang <huanhzhang@tencent.com>
2020-03-02 19:03:14 +0800

    新增类PidWriter来演示gateway的使用 在大多数语言中，通常使用标准库或常用库来访问数据库和文件系统。这些库的api通常是为了便于在对象模型中使用而定义的; 但是，通常不支持编写solitary测试。出于这个原因，我总是建议使用gateway包装常用的库，gateway提供以下功能: • 在solitary测试中禁止访问的能力 • 在每次sociable测试之前重置到基本状态的能力
    初始代码的问题：如果PidWriter包含一个错误和一个先前保存的sample.pid文件存在，断言将继续通过。同样的，如果sample.pid文件在测试过程中被其他进程覆盖，即使PidWriter正常工作，测试也会失败。

---
+++ src/main/java/com/ep/PidWriter.java
@@ -0,0 +1,32 @@
+package com.ep;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.management.RuntimeMXBean;
+
+public class PidWriter {
+    public static void writePid(
+            String filename,
+            RuntimeMXBean bean) {
+        try {
+            writePidtoFile(filename, bean);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static void writePidtoFile(
+            String filename,
+            RuntimeMXBean bean) throws IOException {
+        FileWriter writer =
+                new FileWriter(filename);
+        try {
+            String runtimeName = bean.getName();
+            writer.write(
+                    runtimeName.substring(
+                            0, runtimeName.indexOf('@')));
+        }
+        finally {
+            writer.close();
+        }
+    }
+}
---
+++ src/test/java/com/ep/PidWriterTest.java
@@ -0,0 +1,28 @@
+package com.ep;
+
+import org.junit.Test;
+
+import java.lang.management.RuntimeMXBean;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class PidWriterTest {
+    @Test
+    public void writePid() throws Exception {
+        RuntimeMXBean bean =
+                mock(RuntimeMXBean.class);
+        when(bean.getName()).thenReturn("12@X");
+        PidWriter.writePid(
+                "/tmp/sample.pid", bean);
+        assertEquals(
+                "12",
+                Files.readAllLines(
+                        Paths.get("/tmp/sample.pid"),
+                        Charset.defaultCharset()).get(0));
+    }
+}
